677e2dc08603d429596e6e06b464a597fef0bfb0
amaumene <alexandre@maumene.org>
2026-03-24 10:02:34 +0900

    fix zip to pickle extension swap, remove unused import

--- svc/scrape_property_data.py
+++ svc/scrape_property_data.py
@@ -1,4 +1,3 @@
-import datetime
 import io
 import os
 import zipfile
@@ -12,30 +11,42 @@ def main(event, context):
     """Scrapes the property data from the zipped html data into a dataframe
     and uploads it as a pickle to the same bucket.
     """
-    logger = setup_logger("scrape-property-data", include_timestamp=False, propagate=False)
+    logger = setup_logger(
+        "scrape-property-data", include_timestamp=False, propagate=False
+    )
 
     output_bucket = os.environ["OUTPUT_BUCKET"]
     html_file_fetched_at = event["timestamp"]
     raw_data_key = event["raw_data_key"]
-    scraped_data_key = raw_data_key.replace(".zip", ".pickle")
+    base, _, _ = raw_data_key.rpartition(".zip")
+    scraped_data_key = (
+        f"{base}.pickle" if base else raw_data_key.replace(".zip", ".pickle")
+    )
 
     s3_client = boto3.client("s3")
 
     with io.BytesIO() as stream:
-        s3_client.download_fileobj(Bucket=output_bucket, Key=raw_data_key, Fileobj=stream)
+        s3_client.download_fileobj(
+            Bucket=output_bucket, Key=raw_data_key, Fileobj=stream
+        )
         with zipfile.ZipFile(stream) as zfile:
-            filenames = sorted((zi for zi in zfile.infolist()), key=lambda zi: zi.filename)
+            filenames = sorted(
+                (zi for zi in zfile.infolist()), key=lambda zi: zi.filename
+            )
         # TODO: joblib runs in sequential even for n_jobs > 1 due to limitations
         #   of multiprocessing module in AWS Lambda
-        properties = scrape_properties_from_files(filenames, stream,
-                                                  logger=logger, n_jobs=1)
+        properties = scrape_properties_from_files(
+            filenames, stream, logger=logger, n_jobs=1
+        )
 
     df = make_properties_dataframe(properties, html_file_fetched_at, logger)
 
     with io.BytesIO() as stream:
         df.to_pickle(stream, compression=None, protocol=5)
         stream.seek(0)
-        s3_client.upload_fileobj(Fileobj=stream, Bucket=output_bucket, Key=scraped_data_key)
+        s3_client.upload_fileobj(
+            Fileobj=stream, Bucket=output_bucket, Key=scraped_data_key
+        )
 
     event["scraped_data_key"] = scraped_data_key
     return event
